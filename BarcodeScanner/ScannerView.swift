bc11c5c4ef733c8eaa12d7a82c0b0c0936afd5b9
temoorx <taak140@gmail.com>
2024-04-20 15:13:04 +0500

    vc updated

---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Mac on 20/04/2024.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
